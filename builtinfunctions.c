3396814b007ce3b11a1e5fb106fce049fe385aa2
Johnny Nguyen <111792553+jnguyen02@users.noreply.github.com>
2023-03-21 21:38:40 -0400

    added pwd also homefeature of cd is bugged

--- builtinfunctions.c
+++ builtinfunctions.c
@@ -9,17 +9,30 @@
 void builtin_cd(char *const *instructions) {
     char *dir = instructions[1];
     if (dir != NULL) {
+        if (chdir(dir) != 0) {
+            printf("!mysh> No such file or directory.\n");
+        }
+    } 
+    else {
+            printf("!mysh> No such file or directory.\n");
+        }
+}
+
+
+    /*
+    
+    char *dir = instructions[1];
+    if (dir != NULL) {
         if (strncmp(dir, "~/", 2) == 0) {
-            // replace the tilde with the value of HOME
             char *home = getenv("HOME");
             if (home == NULL) {
-                printf("!mysh> HOME environment variable missing\n");
-            } else {
-                char newdir[strlen(home) + strlen(dir) - 1];
-                sprintf(newdir, "%s%s", home, dir + 1);
-                if (chdir(newdir) != 0) {
-                    printf("!mysh> No such file or directory.\n");
-                }
+                printf("!mysh> HOME environment variable missing.\n");
+                return;
+            }
+            char newdir[strlen(home) + strlen(dir) - 1];
+            sprintf(newdir, "%s%s", home, dir + 1);
+            if (chdir(newdir) != 0) {
+                printf("!mysh> No such file or directory.\n");
             }
         } else {
             if (chdir(dir) != 0) {
@@ -30,12 +43,22 @@ void builtin_cd(char *const *instructions) {
         char *home = getenv("HOME");
         if (home == NULL) {
             printf("!mysh> HOME environment variable missing.\n");
-        } else {
-            if (chdir(home) != 0) {
-                printf("!mysh> No such file or directory.\n");
-            }
+            return;
+        }
+        if (chdir(home) != 0) {
+            printf("!mysh> No such file or directory.\n");
         }
     }
 }
+*/
 
-void builtin_pwd(){}
+
+
+void builtin_pwd(){
+    char current[2000];
+    if (getcwd(current, sizeof(current)) != NULL) {
+        printf("%s\n", current);
+    } else {
+        perror("!mysh> error w getcwd");
+    }
+}
